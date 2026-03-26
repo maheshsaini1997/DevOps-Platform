cc739a2a7cc107413698a59771360361025a4827
maheshsaini1997 <maheshcsis97@gmail.com>
2026-03-26 23:14:11 +0530

    trigger ecr

--- application-microservices/user-service/app.py
+++ application-microservices/user-service/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import requests
-#simple user service that calls payment service to demonstrate inter-service communication in a microservices architectures
 app = Flask(__name__)
 
 @app.route("/")
