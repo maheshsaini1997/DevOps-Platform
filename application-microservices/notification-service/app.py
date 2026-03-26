c3f1b031c10f80908523590b8f946fd25ddb7ef9
maheshsaini1997 <maheshcsis97@gmail.com>
2026-03-26 23:09:40 +0530

    trigger ecr

--- application-microservices/notification-service/app.py
+++ application-microservices/notification-service/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+#simple notification service to demonstrate inter-service communication in a microservices architecture
 @app.route("/notify")
 def notify():
     return {"notification": "sent"}
