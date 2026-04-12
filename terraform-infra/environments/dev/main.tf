2e56cab5383420bdffc456755ccbcd554f1ea13a
maheshsaini1997 <maheshcsis97@gmail.com>
2026-04-12 15:58:42 +0530

    Add terraform CD Pipeline

--- terraform-infra/environments/dev/main.tf
+++ terraform-infra/environments/dev/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "mahesh-terraform-state-123"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
