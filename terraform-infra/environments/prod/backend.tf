7b6830e03f253ba601d2fd57255f9c0da7634298
maheshsaini1997 <maheshcsis97@gmail.com>
2026-04-25 00:34:11 +0530

    Updated Backend.tf

--- terraform-infra/environments/prod/backend.tf
+++ terraform-infra/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mahesh-terraform-state"
+    bucket = "mahesh-terraform-state-530467655811"
     key    = "prod/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-lock"
