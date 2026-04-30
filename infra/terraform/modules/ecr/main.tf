352cfbe904287102a9feafe084c733bb08475092
maheshsaini1997 <maheshcsis97@gmail.com>
2026-04-30 14:37:12 +0530

    Updated Image

--- infra/terraform/modules/ecr/main.tf
+++ infra/terraform/modules/ecr/main.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "repos" {
   count = length(var.repository_names)
   name = "${var.environment}-${var.repository_names[count.index]}"
   force_delete = true
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
